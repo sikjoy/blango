489e936804ed73cc6aec98f43a9a9b8548792797
Kevin Mathis <kfmathis@gmail.com>
2022-09-08 18:23:23 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 import blog.views
 
 urlpatterns = [
+    path('', blog.views.index),
     path('admin/', admin.site.urls),
-    path("", blog.views.index)
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
